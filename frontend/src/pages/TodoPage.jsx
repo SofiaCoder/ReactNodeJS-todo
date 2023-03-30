3231f09551270d554a9d0f13b19776e6f9f3513b
sofiaCoder <sofia.laas@gmail.com>
2023-03-30 23:45:09 +0200

    TodoForm added

--- frontend/src/pages/TodoPage.jsx
+++ frontend/src/pages/TodoPage.jsx
@@ -1,12 +1,13 @@
 import Todos from "../components/todos/Todos"
-import TodoForm from "../components/todos/TodoForm"
+import { TodoForm } from "../components/Form/todoForm"
 import "./scss/TodoPage.scss"
+import { postTodo } from "../components/todos/functions/postTodo.js"
 
 const TodoPage = () => {
     return(
         <div className="todoPage">
             <h1>Your Todos</h1>
-            <TodoForm />
+            <TodoForm submitFunction={postTodo} btnText="Add"/>
             <Todos />
         </div>
     )
