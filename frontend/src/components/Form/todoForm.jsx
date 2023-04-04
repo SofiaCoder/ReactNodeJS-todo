ded10071d4c4b183cf4ea25e93db5ffb385f6d4a
sofiaCoder <sofia.laas@gmail.com>
2023-04-04 12:02:03 +0200

    Editing todoForm to update after added todo

--- frontend/src/components/Form/todoForm.jsx
+++ frontend/src/components/Form/todoForm.jsx
@@ -1,8 +1,15 @@
 import { useState, useEffect } from "react";
+import { getTodos } from "../todos/functions/getTodos";
 
 const TodoForm = ({title, body, submitFunction, btnText}) => {
     const [todoTitle, setTodoTitle] = useState('')
     const [todoBody, setTodoBody] = useState('')
+    const [respons, setRespons] = useState()
+
+    const fetchTodos = () => {
+        getTodos()
+    }
+    
     
 
     useEffect(() => {
@@ -17,27 +24,29 @@ const TodoForm = ({title, body, submitFunction, btnText}) => {
 
     const submitHandler = async (e) => {
         e.preventDefault()
-        await submitFunction(todoTitle, todoBody)
-        
-    }
-
-    const timeFunction = () => {
+        const respons = await submitFunction(todoTitle, todoBody)
+        setRespons(respons)
         setTimeout(() => {
-            
-        }, 3000)
+            setRespons('')
+            setTodoTitle('')
+            setTodoBody('')
+            window.location.reload();
+        }, 2000)
     }
-
 
     return(
-        <div className="todoForm">
-            <form onSubmit={submitHandler}>
-                <label htmlFor="task">Todo: </label>
-                <input type="text" value={todoTitle} id="task" onChange={(e) => setTodoTitle(e.target.value)} />
-                <label htmlFor="todoText">Description: </label>
-                <input type="text" value={todoBody} id="todoText" onChange={(e) => setTodoBody(e.target.value)} />
-                <button type="submit" id="todoBtn">{btnText}</button>
-            </form>
-        </div>
+        <>
+            <div className="todoForm">
+                <form onSubmit={submitHandler}>
+                    <label htmlFor="task">Todo: </label>
+                    <input type="text" value={todoTitle} id="task" onChange={(e) => setTodoTitle(e.target.value)} />
+                    <label htmlFor="todoText">Description: </label>
+                    <input type="text" value={todoBody} id="todoText" onChange={(e) => setTodoBody(e.target.value)} />
+                    <button type="submit" id="todoBtn">{btnText}</button>
+                </form>
+            </div>
+            <h3>{respons}</h3>
+        </>
     )
 }
 
