c963760e9f5ccc3c5503b88c7103655e074f9fbc
sofiaCoder <sofia.laas@gmail.com>
2023-04-04 12:02:40 +0200

    Edited todo.jsx to update after deleting a todo

--- frontend/src/components/todos/Todos.jsx
+++ frontend/src/components/todos/Todos.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
-import "./scss/handlingTodos.scss"
+import "./scss/Todos.scss"
 import handlingCheckbox from "./functions/handlingCheckbox"
 import deleteTodo from "./functions/deleteTodo"
 import { getTodos } from "./functions/getTodos"
@@ -8,12 +8,14 @@ import { getTodos } from "./functions/getTodos"
 
 const HandlingTodos = () => {
     const [todos, setTodos] = useState()
+    const [respons, setRespons] = useState()
 
-    useEffect(() => {
-        const fetchTodos = async () => {
+    const fetchTodos = async () => {
         const data = await getTodos()
         setTodos(data)
-        }
+    }
+
+    useEffect(() => {
         fetchTodos()
      },[])
     
@@ -21,32 +23,43 @@ const HandlingTodos = () => {
         const id = todo.id
         let newValue = todo.value
       
-        todo.value == 1 ? newValue = 0 : newValue = 1
+        todo.value === 1 ? newValue = 0 : newValue = 1
 
         await handlingCheckbox(id, newValue)
-        getTodos()
     }
 
     const deleteHandler = async (todoID) => {
         const id = todoID
-        await deleteTodo(id)
-        await getTodos() 
+        const respons = await deleteTodo(id)
+        setRespons(respons)
+        scrollTop()
+        setTimeout(() => {
+            fetchTodos()
+            setRespons('')
+        },2000) 
     } 
+
+    const scrollTop = function() {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    };
     
         
     return(
-        <div className="handlingTodos">
-            {todos?.map((todo) => {
-                return (<div className="todoBox" key={todo.id}>
-                    <h3>{todo.task}</h3>
-                    <p>{todo.text}</p>
-                    <p>{todo.id}</p>
-                    <input type='checkbox' value={todo.value} checked={todo.value == '1' ? true : false} onChange={() => {checkboxHandler(todo)}}/>
-                    <Link className="editLink" to={`/Todos/${todo.id}`} state={todo} >Edit</Link>
-                    <button className="deleteBtn" onClick={() => {deleteHandler(todo.id)}}>❌</button>
-                </div>)
-            })}
-        </div>
+        <>
+            <h3>{respons}</h3>
+            <div className="handlingTodos">
+                {todos?.map((todo) => {
+                    return (<div className="todoBox" key={todo.id}>
+                        <h3>{todo.task}</h3>
+                        <p>{todo.text}</p>
+                        <p>{todo.id}</p>
+                        <input type='checkbox' value={todo.value} checked={todo.value === 1 ? true : false} onChange={() => {checkboxHandler(todo)}}/>
+                        <Link className="editLink" to={`/Todos/${todo.id}`} state={todo} >Edit</Link>
+                        <button className="deleteBtn" onClick={() => {deleteHandler(todo.id)}}>❌</button>
+                    </div>)
+                })}
+            </div>
+        </>
     )
 }
 
