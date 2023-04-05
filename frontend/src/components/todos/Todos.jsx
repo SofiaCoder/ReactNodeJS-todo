03199921677fe65bab9e29fb5fa4853e1ec8d1f1
sofiaCoder <sofia.laas@gmail.com>
2023-04-05 19:49:09 +0200

    Layout changes and adding nav to login if no auth

--- frontend/src/components/todos/Todos.jsx
+++ frontend/src/components/todos/Todos.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import "./scss/Todos.scss"
 import handlingCheckbox from "./functions/handlingCheckbox"
 import deleteTodo from "./functions/deleteTodo"
@@ -9,15 +9,24 @@ import { getTodos } from "./functions/getTodos"
 const HandlingTodos = () => {
     const [todos, setTodos] = useState()
     const [respons, setRespons] = useState()
+    const navigate = useNavigate()
 
     const fetchTodos = async () => {
         const data = await getTodos()
+    
+        if(data === 401) {
+            setRespons('You are logged out, redirecting you to login-page')
+            setTimeout(() => {
+                navigate('/')
+            }, 4000)
+            return;
+        }
         setTodos(data)
-    }
+    };
 
     useEffect(() => {
         fetchTodos()
-     },[])
+     },[]);
     
     const checkboxHandler = async (todo) => {
         const id = todo.id
@@ -26,7 +35,8 @@ const HandlingTodos = () => {
         todo.value === 1 ? newValue = 0 : newValue = 1
 
         await handlingCheckbox(id, newValue)
-    }
+        fetchTodos()
+    };
 
     const deleteHandler = async (todoID) => {
         const id = todoID
@@ -37,23 +47,23 @@ const HandlingTodos = () => {
             fetchTodos()
             setRespons('')
         },2000) 
-    } 
+    };
 
     const scrollTop = function() {
         window.scrollTo({top: 0, behavior: 'smooth'});
     };
+
     
-        
     return(
         <>
             <h3>{respons}</h3>
-            <div className="handlingTodos">
+            <div className="todoBoxes">
                 {todos?.map((todo) => {
                     return (<div className="todoBox" key={todo.id}>
+                        <input type='checkbox' value={todo.value} checked={todo.value === 1 ? true : false} onChange={() => {checkboxHandler(todo)}}/>
                         <h3>{todo.task}</h3>
                         <p>{todo.text}</p>
                         <p>{todo.id}</p>
-                        <input type='checkbox' value={todo.value} checked={todo.value === 1 ? true : false} onChange={() => {checkboxHandler(todo)}}/>
                         <Link className="editLink" to={`/Todos/${todo.id}`} state={todo} >Edit</Link>
                         <button className="deleteBtn" onClick={() => {deleteHandler(todo.id)}}>❌</button>
                     </div>)
@@ -61,7 +71,7 @@ const HandlingTodos = () => {
             </div>
         </>
     )
-}
+};
 
 export default HandlingTodos
 
