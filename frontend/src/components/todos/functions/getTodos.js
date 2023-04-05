ed1878a0f629892deda795c362717d3b5e74bbfe
sofiaCoder <sofia.laas@gmail.com>
2023-04-05 19:49:45 +0200

    Returning res.status if 401 added

--- frontend/src/components/todos/functions/getTodos.js
+++ frontend/src/components/todos/functions/getTodos.js
@@ -2,6 +2,9 @@ const getTodos = async () => {
     const res = await fetch('http://localhost:5050/todo', {
         credentials: "include"
     })
+    if (res.status === 401) {
+       return res.status
+    }
     const data = await res.json()
     return data
 }
