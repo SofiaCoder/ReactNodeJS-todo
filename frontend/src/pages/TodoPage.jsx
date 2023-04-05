f66ba91d97f0e9aab422c1ce44bd34099af4532d
sofiaCoder <sofia.laas@gmail.com>
2023-04-05 19:55:41 +0200

    Tailor h1 to username's Todos

--- frontend/src/pages/TodoPage.jsx
+++ frontend/src/pages/TodoPage.jsx
@@ -1,12 +1,13 @@
 import Todos from "../components/todos/Todos"
-import { TodoForm } from "../components/Form/todoForm"
+import { TodoForm } from "../components/Form/TodoForm"
 import "./scss/TodoPage.scss"
 import { postTodo } from "../components/todos/functions/postTodo.js"
 
 const TodoPage = () => {
+    const user = localStorage.getItem('username')
     return(
         <div className="todoPage">
-            <h1>Your Todos</h1>
+            <h1>{user}'s Todos</h1>
             <TodoForm submitFunction={postTodo} btnText="Add"/>
             <Todos />
         </div>
