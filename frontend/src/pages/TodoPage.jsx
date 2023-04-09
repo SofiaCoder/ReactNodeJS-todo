7f594274ada6f257cbdfbcf8a31867a9ead9c3a4
sofiaCoder <sofia.laas@gmail.com>
2023-04-09 21:52:28 +0200

    Added FriendPage

--- frontend/src/pages/TodoPage.jsx
+++ frontend/src/pages/TodoPage.jsx
@@ -2,14 +2,17 @@ import Todos from "../components/todos/Todos"
 import { TodoForm } from "../components/Form/TodoForm"
 import "./scss/TodoPage.scss"
 import { postTodo } from "../components/todos/functions/postTodo.js"
+import { FriendPage } from "./FriendPage"
+
 
 const TodoPage = () => {
     const user = localStorage.getItem('username')
     return(
         <div className="todoPage">
             <h1>{user}'s Todos</h1>
-            <TodoForm submitFunction={postTodo} btnText="Add"/>
-            <Todos />
+            <TodoForm className="todoForm" submitFunction={postTodo} btnText="Add"/>
+            <Todos className="todos" />
+            <FriendPage className="friendPage" />
         </div>
     )
 }
