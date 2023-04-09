33800b20fa5621430312210c4caf11a1265ed019
sofiaCoder <sofia.laas@gmail.com>
2023-04-09 21:50:51 +0200

    added getFriends route, changed getTodos to post

--- backend/server/src/routes/friendRouter.js
+++ backend/server/src/routes/friendRouter.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { addFriend } = require('../controllers/friends/addFriend')
 const { getAllUsers } = require('../controllers/friends/getAllUsers')
+const { getFriends } = require('../controllers/friends/getFriends')
 const { getFriendsTodos } = require('../controllers/friends/getFriendsTodos')
 const { authMiddleware } = require('../middlewares/authMiddleware')
 const friendRouter = express.Router()
@@ -8,7 +9,8 @@ const friendRouter = express.Router()
 friendRouter.use(authMiddleware)
 
 friendRouter.get('/', getAllUsers)
-friendRouter.get('/todos', getFriendsTodos)
+friendRouter.get('/display', getFriends)
+friendRouter.post('/todos', getFriendsTodos)
 friendRouter.post('/', addFriend)
 
 
